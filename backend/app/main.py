448c12e614cee8eb12d60741d20ff60356c90214
= <=>
2026-08-08 16:44:55 +0530

    Fix backend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.auth import router as auth_router
 from app.api.ai import router as ai_router
 from app.api.documents import router as document_router
 from app.database.mongodb import client
-from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -26,19 +28,23 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://ai-customer-support-saas-vhtx.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 app.include_router(auth_router)
 app.include_router(ai_router)
 app.include_router(document_router)
+
 
 @app.get("/")
 def home():
